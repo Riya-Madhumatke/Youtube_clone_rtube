948bb86c4229bbcab5a0cde9a253eeaf2ab99b9c
Riya Madhumatke <riyamadhumatke39@gmail.com>
2026-08-05 15:17:27 +0530

    Fixed comment controller exports

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -1,10 +1,11 @@
 import express from "express";
-import{ getallcomment, postcomment ,editcomment, likeComment,
+import { deletecomment, getallcomment, postcomment ,editcomment, likeComment,
   dislikeComment, reportComment, translateComment} from "../controllers/comment.js";
 
 const routes = express.Router();
 routes.get("/:videoid", getallcomment);
 routes.post("/postcomment", postcomment);
+routes.delete("/deletecomment/:id", deletecomment);
 routes.post("/editcomment/:id", editcomment);
 routes.post("/like/:commentId", likeComment);
 routes.post("/dislike/:commentId", dislikeComment);
